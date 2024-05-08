41ca5352286c1f897692db3e0f4ce54f872d3b5f
moaberglund <moa.21@hotmail.com>
2024-05-08 12:41:09 +0200

    post anrop för registrering

--- routes/login.js
+++ routes/login.js
@@ -20,7 +20,22 @@ const User = require("../models/User");
 //post anrop för REGISTRERING
 router.post("/register", async (req, res) => {
     console.log("Registrering påbörjad")
+    try {
+        const { username, password } = req.body;
 
+        //Validera
+        if (!username || !password) {
+            return res.status(400).json({ error: "Tom input, vänligen fyll i båda fält" })
+        }
+
+        //OK - spara ny användare
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).json({ message: "Användare skapad" })
+
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
 })
 
 //post anrop för INLOGGNING
