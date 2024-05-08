798812c83e7bc8c635e3d559f4a3603c213f5f0d
moaberglund <moa.21@hotmail.com>
2024-05-08 13:30:42 +0200

    menu schema

---
+++ models/Menu.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const MenuSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//export
+const Menu = mongoose.model("User", MenuSchema);
+module.exports = Menu;
