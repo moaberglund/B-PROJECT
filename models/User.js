00cf1447c1b77e76a4884cf8cd4ade541a8a7593
moaberglund <moa.21@hotmail.com>
2024-05-08 11:16:21 +0200

    user schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+//User schema
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+
+
+
+
