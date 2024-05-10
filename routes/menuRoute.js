7c67990d3b452aa02ef74176713616c17ab2cba5
moaberglund <moa.21@hotmail.com>
2024-05-10 11:56:05 +0200

    router post menu

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+//anslutning till databas - Atlas MongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Ansluten till MongoDB!");
+}).catch((error) => {
+    console.error("Error i koppling till databasen...");
+});
+
 //Meny modell
 const Menu = require("../models/Menu");
 
@@ -20,9 +28,26 @@ router.get("/menu", async (req, res) => {
 });
 
 
-
+//importera authenticateToken 
+const authenticateToken = require("../server");
 
 //post på skyddad route - lägg till
+router.post("/menu", authenticateToken, async (req, res) => {
+    try {
+        const { category, name, price, description } = req.body;
+
+        if (!category || !name || !price || !description) {
+            return res.status(400).json({ error: "Tom input, vänligen fyll i alla fält" })
+        }
+
+        const menu = new Menu({ category, name, price, description });
+        await menu.save();
+        res.status(201).json({ message: "Menyinlägg skapat" })
+
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+})
 
 
 
