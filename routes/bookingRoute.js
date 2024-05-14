8d216f215e498615c830d717cb81c368b0eb75ca
moaberglund <moa.21@hotmail.com>
2024-05-14 11:20:58 +0200

    skyddad route

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -15,7 +15,27 @@ router.post("/", createBooking);
 
 //SKYDDAD ROUTE (??)
 //hämta och se bokning
-router.get("/", getBookings);
+router.get("/", authenticateToken, getBookings);
+
+
+ 
+//Validera token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1]; //Token
+
+    //Saknas token?
+    if (token == null) res.status(401).json({ message: "Åtkomst nekad! - saknad av token" });
+
+    //Token OK
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({ message: "Ogiltig JWT!" });
+
+        req.username = username;
+        next();
+    });
+};
+
 
 
 
