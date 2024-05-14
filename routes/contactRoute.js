3e84c1538f3db74c6534d2edc0b5ab05c76810a8
moaberglund <moa.21@hotmail.com>
2024-05-14 11:16:22 +0200

    skyddad route

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -14,7 +14,28 @@ router.post("/", createContact);
 
 //(skyddad route)!!!
 //hämta all kontakt meddelanden
-router.get("/", getContacts);
+router.get("/", authenticateToken, getContacts);
+
+
+
+ 
+//Validera token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1]; //Token
+
+    //Saknas token?
+    if (token == null) res.status(401).json({ message: "Åtkomst nekad! - saknad av token" });
+
+    //Token OK
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({ message: "Ogiltig JWT!" });
+
+        req.username = username;
+        next();
+    });
+};
+
 
 
 
